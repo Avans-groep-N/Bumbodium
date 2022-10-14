ecf4ff9dee67ae7e006ecf8a8884e920d9776ab3
Eva van Gurp <imcheeze115@gmail.com>
2022-10-15 01:54:34 +0200

    feat1 - Page for weekly schedule made

---
+++ Bumbodium.Data/Tasks.cs
@@ -0,0 +1,7 @@
+﻿namespace Bumbodium.Data
+{
+    public class Tasks
+    {
+        //TODO: implement (mag gijs lekker doen), connect with database
+    }
+}
---
+++ Bumbodium/Controllers/WeekScheduleController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bumbodium.Controllers
+{
+    public class WeekScheduleController : Controller
+    {
+        // GET: WeekRoosterController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: WeekRoosterController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: WeekRoosterController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WeekRoosterController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WeekRoosterController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WeekRoosterController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WeekRoosterController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WeekRoosterController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
