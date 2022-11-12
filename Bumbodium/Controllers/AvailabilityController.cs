45000151f6521f57c2d68a3fc892eaf8fc960fa5
Martijn1030 <72139731+Martijn1030@users.noreply.github.com>
2022-11-12 17:54:05 +0100

    Availability start
    Adds a scheduler for the availability and the ability to add to the in memory availability schedule

---
+++ Bumbodium/Controllers/AvailabilityController.cs
@@ -0,0 +1,18 @@
+﻿using Bumbodium.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bumbodium.WebApp.Controllers
+{
+    public class AvailabilityController : Controller
+    {
+        // GET: WeekRoosterController
+        public ActionResult Index()
+        {
+            var Availability = new Availability[] { new Availability() {
+                StartDateTime = DateTime.Now,
+                EndDateTime = DateTime.Now.AddHours(1),
+                Type = BeschikbaarheidType.Schoolhours} };
+            return View(Availability);
+        }
+    }
+}
