efe94986491e8d83b5c1d77e0639a0776f813610
GijsVDVelden <gj.vandervelden1@student.avans.nl>
2022-11-13 22:42:15 +0100

    Signup Controller
    created a Signup Controller class

---
+++ Bumbodium/Controllers/SignupController.cs
@@ -0,0 +1,15 @@
+﻿using Bumbodium.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bumbodium.WebApp.Controllers
+{
+    public class SignupController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+    }
+}
