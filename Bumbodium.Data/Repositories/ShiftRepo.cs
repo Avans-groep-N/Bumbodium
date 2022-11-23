eaab745adb5701159235f406f956ef2eb52aa410
kjellgeerlings <k.geerlings@student.avans.nl>
2022-11-23 11:56:56 +0100

    added the Repo for shift

---
+++ Bumbodium.Data/Repositories/ShiftRepo.cs
@@ -0,0 +1,29 @@
+﻿using Bumbodium.Data.DBModels;
+using Bumbodium.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bumbodium.Data.Repositories
+{
+    public class ShiftRepo
+    {
+        private readonly ISqlDataAccess _db;
+        public ShiftRepo(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+        
+        public Task<List<Shift>> GetShifts()
+        {
+            return null;
+        }
+        //add a filter based on department in the function
+        public Task<List<Shift>> GetFilteredShifts()
+        {
+            return null;
+        }
+    }
+}
