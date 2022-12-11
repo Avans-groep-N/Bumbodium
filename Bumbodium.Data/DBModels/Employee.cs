3464e8d045747831fad68baeb4dbf24947767805
GijsVDVelden <gj.vandervelden1@student.avans.nl>
2022-12-12 00:00:34 +0100

    agevalidation init

--- Bumbodium.Data/DBModels/Employee.cs
+++ Bumbodium.Data/DBModels/Employee.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Bumbodium.Data.Utilities;
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -21,6 +22,7 @@ namespace Bumbodium.Data.DBModels
         public string LastName { get; set; }
 
         [Required]
+        [ValidateEmployeeAgeAttribute(15)]
         public DateTime Birthdate { get; set; }
 
         [Required]
