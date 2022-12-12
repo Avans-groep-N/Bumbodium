16b35d855ad3c7471e41d1d61b914bfe17c362ad
GijsVDVelden <gj.vandervelden1@student.avans.nl>
2022-12-12 01:11:20 +0100

    clockingtimesinit

---
+++ Bumbodium/Controllers/ClockingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bumbodium.WebApp.Controllers
+{
+    public class ClockingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
