239903ca95e2f39ad52f8c89d9bdd9534e7e27c7
GijsVDVelden <gj.vandervelden1@student.avans.nl>
2022-12-15 11:59:46 +0100

    add viewmodels

--- Bumbodium/Controllers/ClockingController.cs
+++ Bumbodium/Controllers/ClockingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bumbodium.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bumbodium.WebApp.Controllers
 {
@@ -6,7 +7,38 @@ namespace Bumbodium.WebApp.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var clockingViewModel = new ClockingViewModel() { WeekNumber = 48};
+            clockingViewModel.ClockingItems.Add(new ClockingItemViewModel()
+            {
+                ClockStartTime = new DateTime(2022, 12, 1, 12, 54, 0),
+                ClockEndTime = new DateTime(2022, 12, 1, 18, 12, 0),
+                IsChanged = false,
+                IsOnGoing = false,
+                ScheduleStartTime = new DateTime(2022, 12, 1, 13, 00, 0),
+                ScheduleEndTime = new DateTime(2022, 12, 1, 18, 00, 0)
+
+            });
+            clockingViewModel.ClockingItems.Add(new ClockingItemViewModel()
+            {
+                ClockStartTime = new DateTime(2022, 12, 4, 12, 57, 0),
+                ClockEndTime = new DateTime(2022, 12, 4, 19, 00, 0),
+                IsChanged = false,
+                IsOnGoing = false,
+                ScheduleStartTime = new DateTime(2022, 12, 4, 13, 00, 0),
+                ScheduleEndTime = new DateTime(2022, 12, 4, 18, 00, 0)
+
+            });
+            clockingViewModel.ClockingItems.Add(new ClockingItemViewModel()
+            {
+                ClockStartTime = new DateTime(2022, 12, 5, 12, 51, 0),
+                ClockEndTime = new DateTime(2022, 12, 5, 18, 04, 0),
+                IsChanged = false,
+                IsOnGoing = false,
+                ScheduleStartTime = new DateTime(2022, 12, 5, 13, 00, 0),
+                ScheduleEndTime = new DateTime(2022, 12, 5, 18, 00, 0)
+
+            });
+            return View(clockingViewModel);
         }
     }
 }
---
+++ Bumbodium/Models/ClockingViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Bumbodium.WebApp.Models
+{
+    public class ClockingViewModel
+    {
+
+        public List<ClockingItemViewModel> ClockingItems { get; set; }
+        public ClockingViewModel()
+        {
+            ClockingItems = new List<ClockingItemViewModel>();
+        }
+
+        public string EmployeeName { get; set; }
+
+        public int WeekNumber { get; set; }
+
+
+    }
+}
