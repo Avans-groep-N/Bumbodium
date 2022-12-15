e9992ca781f1493ff4f20074ce56745925140e85
kjellgeerlings <k.geerlings@student.avans.nl>
2022-12-15 11:48:10 +0100

    added checks on businesslayer

---
+++ Bumbodium/Models/BusinessLayer/ScheduleApproval.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Bumbodium.WebApp.Models.BusinessLayer
+{
+    public class ScheduleApproval : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if((value as string).Contains("pain peko"))
+            {
+                return new ValidationResult($"suck my dick");
+            }
+            return ValidationResult.Success;
+
+        }
+
+
+    }
+}
---
+++ Bumbodium/Models/ShiftViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Bumbodium.Data.DBModels;
+using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bumbodium.WebApp.Models
+{
+    public class ShiftViewModel
+    {
+        [Range(typeof(DateTime), "01/01/0000", "01/01/5000", ErrorMessage = "cannot be added in the past")]
+        public DateTime ShiftStartDateTime { get; set; }
+    }
+}
