4c15916fee1a7e6091d9495a5f918fbc0753c090
Martijn1030 <72139731+Martijn1030@users.noreply.github.com>
2023-01-04 19:49:14 +0100

    progress (poggers)

--- Bumbodium/Controllers/EmployeeController.cs
+++ Bumbodium/Controllers/EmployeeController.cs
@@ -1,7 +1,10 @@
 ﻿using Bumbodium.Data;
+using Bumbodium.Data.DBModels;
 using Bumbodium.WebApp.Models;
+using Bumbodium.WebApp.Models.ExcelExport;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Data;
 
 namespace Bumbodium.WebApp.Controllers
@@ -18,10 +21,36 @@ namespace Bumbodium.WebApp.Controllers
 
         public IActionResult Index()
         {
+            IQueryable<Employee> employees = _employeeRepo.GetEmployees().Include(e => e.PartOFDepartment).ThenInclude(pod => pod.Department).OrderBy(e => e.FirstName);
+            int employeesPerPage = 3;
             return View(new EmployeeIndexViewModel()
             {
-                Employees = _employeeRepo.GetEmployees()
-            });
+                CurrentPage = 1,
+                EmployeesPerPage = employeesPerPage,
+                Employees = employees.Take(employeesPerPage),
+                EmployeeCount = employees.Count()
+            }) ;
+        }
+
+        [HttpPost]
+        public IActionResult Index(EmployeeIndexViewModel viewModel)
+        {
+            IQueryable<Employee> employees = _employeeRepo.GetEmployees()
+                .Include(e => e.PartOFDepartment).ThenInclude(pod => pod.Department)
+                .Where(e => e.DateOutService.HasValue == viewModel.ShowInactive);
+            if(!string.IsNullOrEmpty(viewModel.NameFilter))
+            {
+                employees = employees.Where(e => (e.FirstName + " " + e.MiddleName + " " + e.LastName).ToLower().Contains(viewModel.NameFilter.ToLower()));
+            }
+            if (viewModel.DepartmentIdFiter > 0)
+            {
+                employees = employees.Where(e => e.PartOFDepartment.Any(pod => pod.DepartmentId == viewModel.DepartmentIdFiter));
+            }
+            viewModel.EmployeeCount = employees.Count();
+            viewModel.Employees = employees.OrderBy(e => e.FirstName)
+                .Skip(viewModel.EmployeesPerPage * (viewModel.CurrentPage - 1))
+                .Take(viewModel.EmployeesPerPage);
+            return View(viewModel);
         }
     }
 }
--- Bumbodium/Models/EmployeeIndexViewModel.cs
+++ Bumbodium/Models/EmployeeIndexViewModel.cs
@@ -7,12 +7,11 @@ namespace Bumbodium.WebApp.Models
         public IEnumerable<Employee> Employees { get; set; }
         public int EmployeeCount { get; set; }
         public int CurrentPage { get; set; }
-        public int PageCount => EmployeeCount / 5;
+        public int EmployeesPerPage { get; set; }
+        public int PageCount => EmployeeCount / EmployeesPerPage;
 
         public string? NameFilter { get; set; }
-        public int? AgeFilter { get; set; }
-        public IEnumerable<int>? DepartmentsIdFiter { get; set; }
-        public TypeStaff? TypeFilter { get; set; }
-        public bool DateOutService { get; set; }
+        public int? DepartmentIdFiter { get; set; }
+        public bool ShowInactive { get; set; }
     }
 }
