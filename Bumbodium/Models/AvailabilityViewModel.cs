db7d387b589ac06e6424abc2950e14fc210f3d5a
kjellgeerlings <k.geerlings@student.avans.nl>
2023-01-07 15:17:04 +0100

    using radzen scheduler now

--- Bumbodium/Models/AvailabilityViewModel.cs
+++ Bumbodium/Models/AvailabilityViewModel.cs
@@ -3,31 +3,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Bumbodium.WebApp.Models
 {
-    public class AvailabilityViewModel : IValidatableObject
+    public class AvailabilityViewModel 
     {
         [Required]
         public DateOnly Date { get; set; }
         [Required]
         public TimeOnly StartTime { get; set; }
 
-        [Required] 
+        [Required]
+        //TODO: add custom validation
         public TimeOnly EndTime { get; set; }
 
         [Required]
         public AvailabilityType AvailabilityType { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if(StartTime > EndTime)
-            {
-                //TODO: add appropriate message
-                yield return new ValidationResult("");
-            }
-            if(StartTime == EndTime)
-            {
-                yield return new ValidationResult("");
-            }
-            //TODO: add minimum amount of time (maybe)
-        }
     }
 }
