648a8861769781833d46d8b97ab6c2ab439de97b
GijsVDVelden <gj.vandervelden1@student.avans.nl>
2023-01-25 13:24:37 +0100

    init

---
+++ Bumbodium/Controllers/ManagerScheduleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bumbodium.WebApp.Controllers
+{
+    public class ManagerScheduleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bumbodium/Models/DayShiftsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Bumbodium.WebApp.Models
+{
+    public class DayShiftsViewModel
+    {
+
+        public List<ShiftVM> Planning;
+        public DateTime Day;
+        public DateTime OpenTime;
+        public DateTime ClosingTime;
+
+        public DayShiftsViewModel()
+        {
+            Planning = new List<ShiftVM>();
+        }
+
+    }
+}
