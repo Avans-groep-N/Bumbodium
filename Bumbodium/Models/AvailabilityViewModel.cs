186d55e396d0a31ac16f5fd782f4face9b7c2066
kjellgeerlings <k.geerlings@student.avans.nl>
2023-01-30 14:32:46 +0100

    removed ability to edit submit button doesn't work due to validateableobject

--- Bumbodium/Models/AvailabilityViewModel.cs
+++ Bumbodium/Models/AvailabilityViewModel.cs
@@ -1,20 +1,49 @@
-﻿using Bumbodium.Data.DBModels;
+﻿using Bumbodium.Data;
+using Bumbodium.Data.DBModels;
+using Bumbodium.Data.Utilities.EmployeeValidation;
+using Bumbodium.WebApp.Models.Utilities.AvailabilityValidation;
 using System.ComponentModel.DataAnnotations;
 
 namespace Bumbodium.WebApp.Models
 {
-    public class AvailabilityViewModel 
+    public class AvailabilityViewModel : IValidatableObject
     {
         [Required]
+        //[ValidateDate]
         public DateOnly Date { get; set; }
         [Required]
         public TimeOnly StartTime { get; set; }
 
         [Required]
-        //TODO: add custom validation
         public TimeOnly EndTime { get; set; }
 
         [Required]
         public AvailabilityType AvailabilityType { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            //Db doohickery
+            var _ctx = (BumbodiumContext)validationContext
+                         .GetService(typeof(BumbodiumContext));
+            var _availabilityRepo = new AvailabilityRepo(_ctx);
+
+            //Verify that availability can only be added if startTime is before EndTime
+            if (EndTime.CompareTo(StartTime) == -1)
+                yield return new ValidationResult("End time cannot be before start time", new[] { "StartTime" });
+
+            //Verify that user cannot have multiple availabilities on the same time and day
+            var availabilities = _availabilityRepo.GetAvailabilitiesInRange(Date.ToDateTime(StartTime), Date.ToDateTime(EndTime));
+            if (availabilities.Count > 0)
+                yield return new ValidationResult("Cannot have multiple availabilities at the same time", new[] { "StartTime" });
+
+            //Verify that user cannot add availability without a 2 week notice
+            if (Date.Day < DateTime.Today.AddDays(14).Day)
+                yield return new ValidationResult("Cannot add availability under 2 weeks in advance", new[] { "StartTime" });
+
+            //Verify that user cannot add availability in the past
+            if (DateTime.Now.CompareTo(StartTime) > 0)
+                yield return new ValidationResult("Cannot add availability in the past", new[] { "StartTime" });
+        }
     }
 }
+
---
+++ Bumbodium/Models/Utilities/AvailabilityValidation/ValidateDateAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bumbodium.WebApp.Models.Utilities.AvailabilityValidation
+{
+    public class ValidateDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value,
+                ValidationContext validationContext)
+        {
+            DateTime newvalue = Convert.ToDateTime(value.ToString());
+            if(newvalue < DateTime.Today.AddDays(14))
+            {
+                return new ValidationResult("Cannot add availability under 2 weeks in advance");
+            }
+            return ValidationResult.Success;
+
+        }
+    }
+}
